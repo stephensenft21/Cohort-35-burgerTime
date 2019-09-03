73a8b29c3f7a2071f206b964cb34e33062736a31
Stephen Senft <stephensenft21@gmail.com>
2019-09-03 13:36:09 -0500

    completed function

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,63 @@
 const mainContainer = document.getElementById("burger--baby")
 console.log("hello mainContainer", mainContainer)
+
+
+const hamburger = {
+    name: 'Hamburger',
+    type: 'beef',
+    cooked: false,
+}
+const zucchini = {
+    name: 'Zucchini',
+    type: 'vegetable',
+    cooked: false,
+}
+const chickenBreast = {
+    name: 'Chicken Breast',
+    type: 'chicken',
+    cooked: false,
+}
+const corn = {
+    name: 'Corn',
+    type: 'vegetable',
+    cooked: false,
+}
+const steak = {
+    name: 'Steak',
+    type: 'beef',
+    cooked: false,
+}
+
+
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+const cookedFood = [];
+
+
+
+
+
+for (let i = 0; i < foods.length; i++) {
+    function grill(currentObject) {
+        // Modify the food so that it is cooked
+        currentObject.cooked = true;
+
+        // Put the cooked food into the appropriate array
+        cookedFood.push(currentObject);
+
+    };
+
+}
+
+foods.forEach(food => {
+    grill(food)
+
+});
+
+
+
+
+
+
+
+console.log(cookedFood)
